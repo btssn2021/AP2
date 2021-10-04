23d5dc397ce72f1efa918c5132ed9aca32f5b268
eric <eric.thomas@formation-industries-fc.fr>
2021-10-04 09:18:36 +0200

    ex2

--- main.c
+++ main.c
@@ -2,14 +2,27 @@
 
 int main() {
 /***********************************************Ex1****************************************/
-int n1=0,n2=0;
-printf("Veuillez saisir deux nombres entiers\n");
-scanf("%d %d",&n1,&n2);
-if(n1==n2)
-    printf("n1 et n2 sont égaux");
+    int n1=0,n2=0;
+    printf("Veuillez saisir deux nombres entiers\n");
+    scanf("%d %d",&n1,&n2);
+    if(n1==n2)
+        printf("n1 et n2 sont égaux\n");
     else
-    printf("n1 et n2 sont différents");
+        printf("n1 et n2 sont différents\n");
 /***********************************************Ex2*****************************************/
+    int n3=0;
+    printf("Veuillez taper un nombre entier");
+    scanf("%d",&n3);
+    if(n3%2==0)
+    {
+        printf("le nombre est pair\n");
+    }
+    else
+    {
+        printf("le nombre est impair\n");
+    }
+/***********************************Ex3**************************************/
+
 
     return 0;
 }
