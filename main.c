cc4b878d5e52f9fb7faeec133ada644a5ec5fea2
eric <eric.thomas@formation-industries-fc.fr>
2021-10-04 08:59:52 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+/***********************************************Ex1****************************************/
+int n1=0,n2=0;
+printf("Veuillez saisir deux nombres entiers\n");
+scanf("%d %d",&n1,&n2);
+if(n1==n2)
+    printf("n1 et n2 sont égaux");
+    else
+    printf("n1 et n2 sont différents");
+/***********************************************Ex2*****************************************/
+
+    return 0;
+}
